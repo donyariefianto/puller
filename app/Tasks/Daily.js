674b8fcea04ce5448e0b342f2c805104ff85693f
donyariefianto <donyariefianto98@gmail.com>
2023-09-06 16:40:35 +0700

    Update Daily.js

--- app/Tasks/Daily.js
+++ app/Tasks/Daily.js
@@ -6,17 +6,22 @@ const Env = use('Env')
 const project = Env.get('SERVICE_PROJECT')
 class Daily extends Task {
   static get schedule () {
-    return '0 0 23,9 * * *'
+    return '0 0 23,10 * * *'
   }
 
   async handle () {
     if (project==='enygma') {
       await Daily_Task.GenerateMonipad();//tidak lemot
+      console.log('monipad is running daily');
       await Daily_Task.Maritim_Meta();//sekitar 4 menit
+      console.log('maritim meta is running daily');
       await Daily_Task.Maritim_Viewboard();//sekitar 5 menit
+      console.log('maritim viewboard is running daily');
       // await Daily_Task.GenerateStatsDatasets();//sekitar 1 menitan dengan hasil json seperti viewboard
       await Daily_Task.BackupDailyArtemis()//sekitar  8 detik
+      console.log('artmeis is running daily');
       await Daily_Task.SiskaperbapoByMetadata();//sekitar 14 menit
+      console.log('siskaperbapo is running daily');
     }
     if (project==='kaltim') {
       await Daily_Task.Laminetam();
